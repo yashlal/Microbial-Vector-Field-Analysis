dc09e6f5facf696ab20ca8f6e8e9b0f43667b024
yashlal <yash.lal.m@gmail.com>
2022-03-22 09:58:31 -0700

    updated lstm script

--- lstm.py
+++ lstm.py
@@ -17,13 +17,15 @@ def create_inout_sequences(iners_col, tw, p_labels):
             if all([k1>=0 for k1 in in_l]) and all([k2>=0 for k2 in out_val]):
                 train_inout_seq.append((in_l, out_val))
 
+    test_seqs = []
     for j in range(len(iners_col)-2*tw):
         iners_l2 = iners_col[j:j+2*tw]
         check_l2 = p_labels[j:j+2*tw]
         if all([k3>=0 for k3 in iners_l2]) and all([check_l2[q]==check_l2[0] for q in range(len(check_l2))]):
             test_seq = (iners_col[j:j+tw], iners_col[j+tw:j+2*tw])
+            test_seqs.append(test_seq)
 
-    return train_inout_seq, test_seq
+    return train_inout_seq, test_seqs
 
 class LSTM(nn.Module):
     def __init__(self, input_size=1, hidden_layer_size=100, output_size=1):
@@ -56,7 +58,9 @@ for i in range(len(all_data)):
 p_labels = vag_data['meta_Participant'].values.tolist()
 
 train_window = 14
-train_inout_seq, test_seq = create_inout_sequences(all_data, train_window, p_labels)
+train_inout_seq, test_seqs = create_inout_sequences(all_data, train_window, p_labels)
+
+test_seq = test_seqs[0]
 
 test_in = test_seq[0]
 test_out = test_seq[1]
@@ -90,6 +94,7 @@ def main(hsize=100, num_epochs=50, LR=0.01):
     test2_loss_list = []
 
     for i in range(epochs):
+        train_graph = []
         epoch_loss = []
         for seq, labels in train_inout_seq:
             seq = torch.FloatTensor(seq).to(device)
@@ -107,8 +112,11 @@ def main(hsize=100, num_epochs=50, LR=0.01):
             optimizer.step()
 
             epoch_loss.append(train_loss)
+            train_graph.append((y_pred.item(), labels.item()))
 
         if i%25 == 1 or i==epochs-1:
+
+            #testing block
             fut_pred = 14
             tensor_true_test_data = torch.FloatTensor(test_out).view(-1,1).to(device)
 
@@ -117,26 +125,61 @@ def main(hsize=100, num_epochs=50, LR=0.01):
 
             test1_loss_list.append((i, test_loss1))
 
+            np_train_graph = np.array(train_graph)
+
             fig1, ax1 = plt.subplots()
-            ax1.plot(tensor_true_test_data.tolist(), label='True')
-            ax1.plot(ap1.tolist(), label='Test1')
+            ax1.plot(np_train_graph[:,0], label='True')
+            ax1.plot(np_train_graph[:,1], label='Train_Pred')
             ax1.legend()
-            ax1.set_title('L. Iners NN Test')
+            ax1.set_title('L. Iners NN Train')
             ax1.set_ylabel('Relative Abundance')
             plt.show()
 
-            print(f'Epoch:{i}   TrainLoss:{train_loss}    TestLoss1:{test_loss1}')
+            # fig2, ax2 = plt.subplots()
+            # ax2.plot(tensor_true_test_data.tolist(), label='True')
+            # ax2.plot(ap1.tolist(), label='Test_Pred')
+            # ax2.legend()
+            # ax2.set_title('L. Iners NN Test')
+            # ax2.set_ylabel('Relative Abundance')
+            # plt.show()
+            fig11, ax11 = plt.subplots()
+            ax11.plot(np_train_graph[0:181,0], label='True')
+            ax11.plot(np_train_graph[0:181,1], label='Train_Pred')
+            ax11.legend()
+            ax11.set_title('L. Iners NN Train')
+            ax11.set_ylabel('Relative Abundance')
+            plt.show()
+
+            fig12, ax12 = plt.subplots()
+            ax12.plot(np_train_graph[350:651,0], label='True')
+            ax12.plot(np_train_graph[350:651,1], label='Train_Pred')
+            ax12.legend()
+            ax12.set_title('L. Iners NN Train')
+            ax12.set_ylabel('Relative Abundance')
+            plt.show()
+
+            fig13, ax13 = plt.subplots()
+            ax13.plot(np_train_graph[750:,0], label='True')
+            ax13.plot(np_train_graph[750:,1], label='Train_Pred')
+            ax13.legend()
+            ax13.set_title('L. Iners NN Train')
+            ax13.set_ylabel('Relative Abundance')
+            plt.show()
+
+            print(f'Epoch:{i}       TestLoss1:{test_loss1}')
+
         loss_avg = sum(epoch_loss)/len(epoch_loss)
         train_loss_list.append(loss_avg)
+
     test1_loss_list = np.array(test1_loss_list)
 
-    fig2, ax2 = plt.subplots()
-    ax2.plot(train_loss_list)
-    ax2.set_title('Training Loss')
+    fig3, ax3 = plt.subplots()
+    ax3.plot(train_loss_list)
+    ax3.set_title('Training Loss')
     plt.show()
 
-    fig3, ax3 = plt.subplots()
-    ax3.plot(test1_loss_list[:,0], test1_loss_list[:,1], label='Test1')
-    ax3.legend()
-    ax3.set_title('Testing Loss')
+    fig4, ax4 = plt.subplots()
+    ax4.plot(test1_loss_list[:,0], test1_loss_list[:,1], label='Test1')
+    ax4.legend()
+    ax4.set_title('Testing Loss')
     plt.show()
